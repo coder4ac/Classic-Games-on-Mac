cdcb0f9f34b70764b6a336bc69b74bf70e7adda4
Sihang Wang <sihangwang@mines.edu>
2024-08-18 08:49:47 -0600

    Modified snake game view, integrated game board. Now available to play.

---
+++ ClassicGames/ClassicGames/GameBoardView.swift
@@ -0,0 +1,18 @@
+//
+//  GameBoardView.swift
+//  ClassicGames
+//
+//  Created by sihang wang on 8/17/24.
+//
+
+import SwiftUI
+
+struct GameBoardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameBoardView()
+}
---
+++ ClassicGames/ClassicGames/SnakeGameModel.swift
@@ -0,0 +1,8 @@
+//
+//  SnakeGameModel.swift
+//  ClassicGames
+//
+//  Created by sihang wang on 8/16/24.
+//
+
+import Foundation
